9355decd6ea9969f84242c69b45cc5820664b19e
jmattaa <mattajonathan1@gmail.com>
2023-12-31 16:03:27 +0100

    add tnn alias
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,6 +8,23 @@ tn() {
     tmux new -s "$session_name"
 } 
 
+tnn() {
+    if [ -z "$1" ]; then
+        session_name="$(basename "$(pwd)")"
+    else
+        session_name="$1-$(basename "$(pwd)")"
+    fi
+
+    tmux new-session -d -s "$session_name" "nvim ."
+    tmux new-window -t "$session_name"
+
+    # focus on the nvim win
+    tmux select-window -t "$session_name:0"
+
+    # attach to the tmux session
+    tmux attach -t "$session_name" 
+}
+
 alias fcd="source ~/.fcd/fcd"
 
 # fuck
