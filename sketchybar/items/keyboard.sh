0f8aac04ed4fd2b81fe3c404591d43d97e92dd74
jmattaa <mattajonathan1@gmail.com>
2025-03-20 23:29:18 +0100

    rm sketchybar, alacritty, qutebrowsre, kitty and yabai

--- sketchybar/items/keyboard.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item keyboard left \
-           --set keyboard label.color=$ACCENT_COLOR \
-                       script="$PLUGIN_DIR/keyboard.sh"
--- sketchybar/plugins/keyboard.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-layout="$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist \
-    AppleSelectedInputSources | 
-    grep "KeyboardLayout" | 
-    awk '/KeyboardLayout Name/ {print $NF}' | tr -d ';'
-)"
-
-sketchybar --set $NAME label=$layout
