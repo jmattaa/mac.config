9ded507ca0e1f3bbc399068a45c1c191efcf795d
jmattaa <mattajonathan1@gmail.com>
2025-06-06 13:58:50 +0200

    refactor color definitions and usage so we can use the colors without icons

--- lsr/colors.lua
+++ lsr/colors.lua
@@ -1,10 +1,21 @@
+Colors = {
+    DIR = "\x1b[34m",
+    SYMLINK = "\x1b[36m",
+    FILE = "\x1b[38m",
+    HIDDEN = "\x1b[90m",
+    EXEC = "\x1b[32;4m",
+    ARCHIVE = "\x1b[31m",
+    MEDIA = "\x1b[33m",
+    DOCUMENT = "\x1b[35;3m"
+}
+
 L_colors = {
-    DIR = "\x1b[34m ",
-    SYMLINK = "\x1b[36m ",
-    FILE = "\x1b[38m ",
-    HIDDEN = "\x1b[90m ",
-    EXEC = "\x1b[32;4m ",
-    ARCHIVE = "\x1b[31m ",
-    MEDIA = "\x1b[33m ",
-    DOCUMENT = "\x1b[35;3m "
+    DIR = Colors.DIR .. " ",
+    SYMLINK = Colors.SYMLINK .. " ",
+    FILE = Colors.FILE .. " ",
+    HIDDEN = Colors.HIDDEN .. " ",
+    EXEC = Colors.EXEC .. " ",
+    ARCHIVE = Colors.ARCHIVE .. " ",
+    MEDIA = Colors.MEDIA .. " ",
+    DOCUMENT = Colors.DOCUMENT .. " "
 }
--- lsr/lsr.lua
+++ lsr/lsr.lua
@@ -7,8 +7,8 @@ function L_long_format(entry, longest_name)
     local owner = string.format("%-" .. longest_name .. "s ", entry.owner)
 
     return string.format("%s %s%s %s%s ",
-        last_modified, L_colors.MEDIA, size,
-        L_colors.SYMLINK, owner)
+        last_modified, Colors.MEDIA, size,
+        Colors.SYMLINK, owner)
 end
 
 L_default_args = {
@@ -17,6 +17,8 @@ L_default_args = {
     directories = true,
     symlinks = true,
     git = { status = false, ignore = false },
+    recursive = false,
+    directory_size = false,
     long = false,
     sort = true,
     filters = {},
